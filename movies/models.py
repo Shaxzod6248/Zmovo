ad9cad9bd7908948d53240d79a612e31f2d26741
Shaxzod <painloversgame@gmail.com>
2022-12-09 22:38:57 +0500

    video qoshildi

--- movies/models.py
+++ movies/models.py
@@ -5,7 +5,7 @@ import os
 
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]
-    valid_extensions = ['.pdf', '.doc', '.docx']
+    valid_extensions = ['.pdf', '.doc', '.docx', '.avi']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
 
@@ -18,11 +18,11 @@ class Category(models.Model):
 
 
 class Movie(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
-    image = models.ImageField(upload_to='zmovi_movies', null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    video = models.FileField(upload_to='videos', null=True)
     title = models.CharField(max_length=300, null=True)
     description = models.TextField(blank=True, null=True)
-    price = models.FloatField()
+    hours = models.CharField(max_length=300, null=True)
 
     def __str__(self):
         return self.title
