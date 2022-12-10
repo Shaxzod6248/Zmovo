6983c042958b4ba66e5b4f37ba7b8d6b962bfa01
Shaxzod <painloversgame@gmail.com>
2022-12-10 14:25:57 +0500

    format togrlandi

--- movies/models.py
+++ movies/models.py
@@ -5,7 +5,7 @@ import os
 
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1]
-    valid_extensions = ['.pdf', '.doc', '.docx', '.avi', '.mp4']
+    valid_extensions = ['.pdf', '.doc', '.docx']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension.')
 
